4f65fb644ca88043315af7b6e598c24adc9ecf27
arunesh12389 <arunesh.22bce7905@vitapstudent.ac.in>
2025-10-05 22:02:48 +0530

    added responsive footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../css/Footer.css"; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section">
+          <h3 className="footer-title">Movie App</h3>
+          <p>All your entertainment. One platform. Free forever.</p>
+          <p className="footer-disclaimer">
+            Movie App does not store any files on our server, we only linked to the media which is hosted on 3rd party services.
+          </p>
+        </div>
+        <div className="footer-section">
+          <h4>Links</h4>
+          <ul>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Follow Us</h4>
+          <div className="footer-socials">
+            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><i className="fab fa-reddit"></i></a>
+            <a href="#"><i className="fab fa-discord"></i></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        © 2025 Movie App. All rights reserved.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
