6e959fb6d24e2a9c31d8e4c79032e3f45bb39a17
Adrian Fernandes <boybothere@gmail.com>
2025-10-15 12:28:52 +0530

    feat: Add Mongoose and configure DB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL')
+      }),
+      inject: [ConfigService]
+    }),
+    UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+
+  constructor(
+    @InjectConnection() private connection: Connection
+  ) {
+    this.checkDBConnection();
+  }
+
+  checkDBConnection() {
+    const state = this.connection.readyState;
+    if (state === 1) console.log("DB connected");
+    else if (state === 0) console.log("DB disconnected");
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
