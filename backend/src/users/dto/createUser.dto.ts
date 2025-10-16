09b2ebd406e9f16e389b47609786575951e34515
Adrian Fernandes <boybothere@gmail.com>
2025-10-17 01:21:21 +0530

    feat(users): add create-user DTO

---
+++ backend/src/users/dto/createUser.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class createUserDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(50, { message: "Username cannot exceed 50 characters" })
+    username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+
+    @IsOptional()
+    @IsString()
+    lastName?: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(5, { message: "Password must have a minimum length of 5 characters" })
+    @MaxLength(20, { message: "Password must have a maximum length of 20 characters" })
+    password: string;
+}
